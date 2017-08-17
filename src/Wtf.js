c33e062d630a26455a3ef78d07f848c8b4fedc17
jesse yuen <contactjesseyuen@gmail.com>
2017-08-17 21:57:02 +0800

    WTF! component created

---
+++ src/Wtf.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class WhatThe extends Component {
+  render() {
+    return (
+      <div className="form-info-wrapper">
+        <a href="" className="form-info-toggle">What's all this then?</a>
+        <div className="form-info"><p>Information about this project will be in a modal or something.</p></div>
+      </div>
+    );
+  }
+}
+
+export default WhatThe;
