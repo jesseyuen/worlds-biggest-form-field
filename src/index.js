58fde99ef0213a5e86119f304e1b087c130e0ed2
jesse yuen <contactjesseyuen@gmail.com>
2017-08-17 21:57:33 +0800

    Rename to App

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import BiggestFormField from './App';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<BiggestFormField />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
 
 registerServiceWorker();
