0df80be95a7942f42ed0322eecc659ac20ea5aee
jesseyuen <hello@jesseyuen.com>
2017-08-18 10:12:26 +0800

    Update Wtf.js

--- src/Wtf.js
+++ src/Wtf.js
@@ -5,7 +5,7 @@ class WhatThe extends Component {
   render() {
     return (
       <div className="form-info-wrapper">
-        <a href="" className="form-info-toggle">What's all this then?</a>
+        <a href="" className="form-info-toggle">Whats all this then?</a>
         <div className="form-info"><p>Information about this project will be in a modal or something.</p></div>
       </div>
     );
