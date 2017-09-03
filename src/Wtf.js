8f8ee712ec5cbe46e8b3fb6a02acfa7fe2114f40
jesse yuen <contactjesseyuen@gmail.com>
2017-09-03 20:23:52 +0800

    Set up class toggle on body

--- src/Wtf.js
+++ src/Wtf.js
@@ -2,15 +2,27 @@ import React, { Component } from 'react';
 import glamorous from 'glamorous';
 
 const WhatThe = glamorous.section({
-  display: 'none'
+  position: 'absolute',
+  bottom: 20,
+  left: 20,
+  zIndex: 10
 })
+
+function toggleDraw(e) {
+  e.preventDefault();
+  if ( document.body.classList.contains('has-open-draw') ) {
+  document.body.classList.remove('has-open-draw');
+  } else {
+    document.body.classList.add('has-open-draw');
+  }
+}
 
 class WhatTheWrapper extends Component {
   render() {
     return (
       <WhatThe>
-        <a href="" className="form-info-toggle">Whats all this then?</a>
-        <div className="form-info"><p>Information about this project will be in a modal or something.</p></div>
+        <a href="" className="form-info-toggle" onClick={toggleDraw}>Whats all this then?</a>
+        {/* <div className="form-info"><p>Information about this project will be in a modal or something.</p></div> */}
       </WhatThe>
     );
   }
