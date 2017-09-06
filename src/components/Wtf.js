937fcafd64cd2a420a39b68bea12a239795812d0
jesse yuen <contactjesseyuen@gmail.com>
2017-09-06 22:26:14 +0800

    Minor stuff and things

--- src/components/Wtf.js
+++ src/components/Wtf.js
@@ -2,18 +2,17 @@ import React, { Component } from 'react';
 import glamorous from 'glamorous';
 
 const WhatThe = glamorous.section({
-  position: 'absolute',
-  bottom: 20,
-  left: 20,
-  zIndex: 10
+  display: 'inline-block'
 })
 
 function toggleDraw(e) {
-  e.preventDefault();
+  e.preventDefault();  
   if ( document.body.classList.contains('has-open-draw') ) {
-  document.body.classList.remove('has-open-draw');
+    document.body.classList.remove('has-open-draw');
+    document.getElementById('form-info-toggle').innerHTML = 'Whats all this then?';
   } else {
     document.body.classList.add('has-open-draw');
+    document.getElementById('form-info-toggle').innerHTML = 'Oh, I see... Weird.';
   }
 }
 
@@ -21,7 +20,7 @@ class WhatTheWrapper extends Component {
   render() {
     return (
       <WhatThe>
-        <a href="" className="form-info-toggle" onClick={toggleDraw}>Whats all this then?</a>
+        <a href="" id="form-info-toggle" onClick={toggleDraw}>Whats all this then?</a>
       </WhatThe>
     );
   }
