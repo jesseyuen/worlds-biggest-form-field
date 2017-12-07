5572a64a33030e0521a67abdc9f7c22f42f1b50d
jesse yuen <contactjesseyuen@gmail.com>
2017-12-08 06:59:35 +0800

    Animated gift shop things coming along.

--- src/components/GiftShop.js
+++ src/components/GiftShop.js
@@ -1,22 +1,39 @@
 import React, { Component } from 'react';
 import glamorous from 'glamorous';
 
+const { css } = glamor
+
+const rotate = css.keyframes({
+  '0%': { transform: `rotate(0deg)` },
+  '100%': { transform: `rotate(360deg)` }
+})
+
 const GiftShop = glamorous.div({
+  
   position: 'fixed',
-  top: 0,
+  top: 40,
   right: 0,
+  width: 200,
+  height: 200,
   color: '#fff',
   zIndex: 1,
+  fontSize: 22,
+  textTransform: 'uppercase',
+  letterSpacing: '0.3em',
+  animation: `${rotate} 30s infinite`,
 
-  // '@media(max-width: 530px)': {
-  //   width: '50%',
-  //   margin: '0',
+  ' #circle': {
+    textAlign: 'center',
+    position: 'absolute',
+    height: '100%',
+    width: '100%',
+    top: 0,
+    left: 0,
+  },
 
-  //   ' span': {
-  //      borderLeft: '0',
-  //      borderRadius: '0 3px 3px 0',
-  //   },
-  // },
+  ' text': {
+    fill: '#fff',
+  }
 })
 
 class GiftShopWrapper extends Component {
@@ -24,15 +41,18 @@ class GiftShopWrapper extends Component {
     return (
       <GiftShop>
         
+       
+        <a href="" title="Visit the gist Shop" target="_blank">
         <svg preserveAspectRatio="meet" viewBox="0 0 250 250">
-        <title>Radial text</title>
-          <path id="circle" d="M0.5,250.5c0,138.07,111.929,250,250,250s250-111.93,250-250 c0-138.071-111.929-250-250-250S0.5,112.429,0.5,250.5" fill="none"></path>
+          <title>Vit rhe Gift Shop</title>
+          <path id="circle" d="M0,100a100,100 0 1,0 200,0a100,100 0 1,0 -200,0" fill="none"></path>
           <text font-size="30">
-            <textpath xlink:href="#circle">
+            <textPath xlinkHref="#circle">
             Gift Shop • Gift Shop • Gift Shop • 
-            </textpath>
+            </textPath>
           </text>
         </svg>
+        </a>
 
       </GiftShop>
     );
